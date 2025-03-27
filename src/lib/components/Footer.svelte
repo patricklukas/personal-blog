b56b5e8eb13b781802875135b51b7fafb5d0f0a1
Patrick Hein <patrick7cb@gmail.com>
2025-03-27 11:25:43 +0100

    update footer content, adjust typography settings, enhance page metadata, and restructure homepage layout; remove first blog post

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,10 @@
 <footer>
-    <small
-        >Made by Patrick Hein. No cookies, no tracking. Enjoy an essential web.</small
-    ><small> <a href="/rss.xml">RSS Feed</a></small>
+  <small
+    >Made with Svelte and vanilla CSS. No cookies, no tracking. Enjoy an
+    essential web.</small
+  >
+  <small
+    >Less, but better |
+    <a href="/rss.xml">RSS Feed</a></small
+  >
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,47 +1,57 @@
 <script lang="ts">
-    import type { PageData } from "./$types";
-    import PostList from "$lib/components/PostList.svelte";
-    export let data: PageData;
+  import type { PageData } from "./$types";
+  import PostList from "$lib/components/PostList.svelte";
+  export let data: PageData;
 </script>
 
 <svelte:head>
-    <title>Pat's Page</title>
-    <meta property="og:title" content="Pat's Page" />
-    <meta name="description" content="A blog about life" />
+  <title>Pat's Page</title>
+  <meta property="og:title" content="Patrick Hein" />
+  <meta
+    name="description"
+    content="Patrick Hein - Advocating for simplicity and the courage to use your own mind"
+  />
 </svelte:head>
 
 <div class="center stack">
-    <div>
-        <div class="sidebar">
-            <div class="sidebar-reverse">
-                <img src="https://placehold.co/300x300" alt="" />
-                <article class="flow">
-                    <h1>Welcome to my piece of the web</h1>
-                    <p>
-                        Over time, I'll be writing about a wide range of topics,
-                        from my projects, philosophy, and life to my work and
-                        achievements.
-                    </p>
-                    <p>
-                        Feel free to browse my <a
-                            data-sveltekit-preload-code
-                            href="/blog">blog</a
-                        >
-                        and
-                        <a data-sveltekit-preload-code href="/about"
-                            >reading list</a
-                        >. Also feel free to get in touch with me!
-                    </p>
-                </article>
-            </div>
-        </div>
+  <div>
+    <!-- <div class="sidebar">
+      <div class="sidebar-reverse">
+        <img src="https://placehold.co/300x300" alt="" /> -->
+    <article class="flow">
+      <h1>Hey, I'm Patrick Hein</h1>
+      <p>
+        Entrepreneur, AI Consultant, Permaculturist and advocate for simplicity
+        and sustainability.
+      </p>
+      <ul>
+        <li>
+          Currently traveling Europe while building Socrates, an approachable
+          and incredibly helpful AI coach.
+        </li>
+        <li>
+          Founder of fivehive, an AI consultancy that helps companies build
+          AI-powered products and services.
+        </li>
+      </ul>
+      <p>
+        Feel free to browse my <a data-sveltekit-preload-code href="/blog"
+          >blog</a
+        >
+        and
+        <a data-sveltekit-preload-code href="/about">reading list</a>. Also feel
+        free to get in touch with me!
+      </p>
+    </article>
+    <!-- </div>
+    </div> -->
+  </div>
+  <hr />
+  <div>
+    <div class="stack">
+      <h2>Latest from the blog</h2>
+      <PostList posts={data.posts} />
     </div>
-    <hr />
-    <div>
-        <div class="stack">
-            <h2>Latest from the blog</h2>
-            <PostList posts={data.posts} />
-        </div>
-    </div>
-    <!-- <hr /> -->
+  </div>
+  <!-- <hr /> -->
 </div>
